42bbc844d2caaa41a5c19d162b446ceb23ea1e55
btran8337 <btran8337@yahoo.com>
2020-09-11 15:47:19 -0500

    updated Users Component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -4,27 +4,27 @@ import Spinner from '../layout/Spinner';
 import GithubContext from '../../context/github/githubContext';
 
 const Users = () => {
-  const githubContext = useContext(GithubContext);
+	const githubContext = useContext(GithubContext);
 
-  const { loading, users } = githubContext;
+	const { loading, users } = githubContext;
 
-  if (loading) {
-    return <Spinner />;
-  } else {
-    return (
-      <div style={userStyle}>
-        {users.map(user => (
-          <UserItem key={user.id} user={user} />
-        ))}
-      </div>
-    );
-  }
+	if (loading) {
+		return <Spinner />;
+	} else {
+		return (
+			<div style={userStyle}>
+				{users.map((user) => (
+					<UserItem key={user.id} user={user} />
+				))}
+			</div>
+		);
+	}
 };
 
 const userStyle = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gridGap: '1rem'
+	display: 'grid',
+	gridTemplateColumns: 'repeat(3, 1fr)',
+	gridGap: '1rem'
 };
 
 export default Users;
